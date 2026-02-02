dcb21ba759b5e299a3bdcfa2e39e33aae9ba827e
tuanbao93 <39887677+tuanbao93@users.noreply.github.com>
2026-02-02 17:58:12 -0600

    update

--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -1,1 +1,13 @@
 
+function Member(){
+
+    return(
+        <div>
+            <h1>Member</h1>
+            <p>This page will let Customer sign in / sign up</p>
+            <p>It's the "Add" function required in the assignment.</p>
+        </div>
+    );
+}
+
+export default Member

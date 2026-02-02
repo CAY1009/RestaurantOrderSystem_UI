256501bff286b371fb9ca4dd23061dd8d6f423fc
tuanbao93 <39887677+tuanbao93@users.noreply.github.com>
2026-02-02 11:46:42 -0600

    Update App.jsx
    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router';
-import Home from './pages/Home';
 import Header from './Header.jsx';
 import Food from './Body.jsx';
 import Footer from './Footer.jsx';

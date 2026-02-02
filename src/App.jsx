75ec0238308bbb8e73f96e5e0ecfb7f2396011b5
tuanbao93 <39887677+tuanbao93@users.noreply.github.com>
2026-02-02 17:50:51 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Header.jsx';
-import Food from './Body.jsx';
+import Food  from './Home.jsx';
 import Footer from './Footer.jsx';
+import Member from './pages/Member.jsx';
 
 function App() {
   
   return (
-    <>
+    <BrowserRouter>
       <Header/>
-      <Food/>
+        <Routes>
+          <Route path='/' element={<Food/>}></Route>
+          <Route path='/member' element={<Member/>}></Route>
+        </Routes>
       <Footer/>
-    </>
+    </BrowserRouter>
   );
 }
 
